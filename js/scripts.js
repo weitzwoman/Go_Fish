e74992199f9757fbf9ea0e4b6e52cfb328871f3e
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-11-04 16:46:17 -0700

    More potential functions added

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,41 @@ function makeDeck() {
     for (var j = 0; j < ranks.length; j++) {
       var newCard = new Card(ranks[j], suits[i]);
       deck.push(newCard);
-      //console.log(newCard);
     }
   }
-
   return deck;
 }
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+makeDeck();
+shuffle(deck);
+
+var player1Hand = [];
+var player2Hand = [];
+
+function deal() {
+  for (var index = 0; index < 7; index++) {
+    player1Hand = deck.pop(index);
+  }
+  return player1Hand;
+}
+
+deal();
