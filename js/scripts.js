171944b8f5093e19bb7aedc20e358f38f23abc7c
Jonathan Thom and Tracie Weitzman <jonathan@>
2016-11-09 14:30:30 -0800

    add delays

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
 var homerState = "";
-
+var output = "";
+var player1TurnResult = "";
+var player2TurnResult = "";
+var player1Counter = 0;
+var player2Counter = 0;
+var bookCreated1 = "";
+var bookCreated2 = "";
+var player1 = new Player();
+var player2 = new Player();
+var player1Wins;
+var player2Wins;
 
 function Card(suit,rank, image) {
   this.suit = suit;
@@ -26,12 +36,9 @@ Game.prototype.makeDeck = function() {
         this.deck.push(newCard);
       }
     }
-
-
   var k = 0;
   var l = 0;
   var temp = null;
-
   for (k = this.deck.length - 1; k > 0; k -= 1) {
     l = Math.floor(Math.random() * (k + 1))
     temp = this.deck[k]
@@ -41,29 +48,22 @@ Game.prototype.makeDeck = function() {
   return this.deck;
 }
 
-var output = "";
 Game.prototype.deal = function() {
   for (var index = 0; index < 5; index++) {
     var deal = this.deck.pop(index);
     this.currentPlayer.hand.push(deal);
   }
   this.currentPlayer = player2;
-
-  /// DON'T REPEAT YOURSELF
   for (var index = 0; index < 5; index++) {
     var deal = this.deck.pop(index);
     this.currentPlayer.hand.push(deal);
   }
   this.currentPlayer = player1;
-
   for (var i=0; i<player1.hand.length; i++) {
     var card1 = player1.hand[i].rank + " of " + player1.hand[i].suit;
     output += card1 + " ";
   }
 }
-
-var player1TurnResult = "";
-var player2TurnResult = "";
 
 Game.prototype.turns = function(request) {
   if (this.currentPlayer === player1) {
@@ -75,14 +75,9 @@ Game.prototype.turns = function(request) {
         player1TurnResult = "Hit! You got a " + player2.hand[i].rank + " from Homer!";
         player2.hand.splice(i, 1);
         checkBook(player1.hand, request, "Player 1");
-
       }
     }
-
-
-
     var draw = [];
-  //  debugger;
     if (goFish.length === 0 && this.deck.length > 0) {
       draw = this.deck.pop(0);
       player1.hand.push(draw);
@@ -98,13 +93,11 @@ Game.prototype.turns = function(request) {
     var card1 = player1.hand[i].rank + " of " + player1.hand[i].suit;
     output += card1 + " ";
   }
-    this.currentPlayer = player2;
-
+  this.currentPlayer = player2;
 
   if (this.currentPlayer === player2) {
     var computerRequest = player2.hand[Math.floor(Math.random() * player2.hand.length)];
     var goFish2 = [];
-
     for (var i = 0; i < player1.hand.length; i++) {
       if (player1.hand[i].rank === computerRequest.rank) {
         player2.hand.push(player1.hand[i]);
@@ -115,7 +108,6 @@ Game.prototype.turns = function(request) {
         homerState = "<img src='img/homerwin.png' alt='picture of Homer celebrating' class='donut'>";
       }
     }
-
     var draw2 = [];
     if (goFish2.length === 0 && this.deck.length > 0) {
       draw2 = this.deck.pop(0);
@@ -131,20 +123,13 @@ Game.prototype.turns = function(request) {
   this.currentPlayer = player1;
 }
 
-var player1Counter = 0;
-var player2Counter = 0;
-var bookCreated1 = "";
-var bookCreated2 = "";
-
 function checkBook(hand, rank, player) {
   var book = [];
   for (var q = 0; q < hand.length; q++) {
     if (hand[q].rank === rank) {
       book.push(hand[q]);
-
     }
   }
-
   if (book.length === 4 && player === "Player 1") {
     bookCreated1 = "You created a book of " + rank + "'s!";
     player1Counter++;
@@ -152,20 +137,13 @@ function checkBook(hand, rank, player) {
     bookCreated2 = "Homer created a book of " + rank + "'s!";
     player2Counter++;
   }
-
-  if (player1Counter >= 1) {
+  if (player1Counter >= 7) {
     player1Wins = true;
   }
-
   if (player2Counter >= 7) {
     player2Wins = true;
   }
 }
-
-var player1 = new Player();
-var player2 = new Player();
-var player1Wins;
-var player2Wins;
 
 // UI
 $(document).ready(function(){
@@ -173,7 +151,6 @@ $(document).ready(function(){
     var game = new Game();
     game.makeDeck();
     game.deal();
-
     var showCards = "";
     for (var a = 0; a < player1.hand.length; a++) {
       var cardImage = "<input type='radio' name='cards' value='" + player1.hand[a].rank + "'><img src='img/" + player1.hand[a].rank + player1.hand[a].suit + ".png' id='resize'>";
@@ -187,21 +164,35 @@ $(document).ready(function(){
     $(".playerWell").show();
     $("#homerPic").html("<img src='img/homer.png' alt='picture of Homer with donut' class='donut'>");
 
-
     $("form").submit(function(event){
       event.preventDefault();
+
       var guess = $("input:radio[name=cards]:checked").val();
-      var turn = game.turns(guess);
+      game.turns(guess);
       $("#player1Turn").text(player1TurnResult);
-      $("#player2Turn").text(player2TurnResult);
+      $("#player2Turn").text("");
+      function player2Turn(player2TurnResult) {
+        $("#player2Turn").text(player2TurnResult);
+      }
+      window.setTimeout(player2Turn, 2000, player2TurnResult);
       $("#dialogueBox").hide();
       $("#userScore").text(player1Counter);
-      $("#computerScore").text(player2Counter);
+      function bookCounterDelay(homerCounter) {
+        $("#computerScore").text(homerCounter);
+      }
+      window.setTimeout(bookCounterDelay, 2000, player2Counter);
       $("#bookCreated1").text(bookCreated1);
       $("#deckCount").text(game.deck.length);
-      $("#homerCards").text(player2.hand.length);
+      function homerCardCounter (length) {
+        $("#homerCards").text(length);
+      }
+      window.setTimeout(homerCardCounter, 2000, player2.hand.length);
       $("#bookCreated2").text(bookCreated2);
-      $("#homerPic").html(homerState);
+      $("#homerPic").html("");
+      function photoDelay(homerState) {
+        $("#homerPic").html(homerState);
+      }
+      window.setTimeout(photoDelay, 2000, homerState);
       var showCards = "";
       for (var a = 0; a < player1.hand.length; a++) {
         var cardImage = "<input type='radio' name='cards' value='" + player1.hand[a].rank + "'><img src='img/" + player1.hand[a].rank + player1.hand[a].suit + ".png' id='resize'>";
@@ -223,6 +214,5 @@ $(document).ready(function(){
         document.location.reload(true);
       });
     });
-
   });
 });
